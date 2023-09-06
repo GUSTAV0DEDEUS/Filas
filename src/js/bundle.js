8927bdfbb7c16aa9f7c7b6cab370a6ce43a1b8c0
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2023-09-06 20:13:57 -0300

    v1.00

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log("hello");
+var videos = document.querySelectorAll(".video");
+videos.forEach(function (video, index) {
+  video.addEventListener("ended", function () {
+    video.pause();
+    video.style.display = "none";
+    if (index < videos.length - 1) {
+      videos[index + 1].style.display = "block";
+      videos[index + 1].play();
+    } else {
+      videos[0].style.display = "block";
+      videos[0].play();
+    }
+  });
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const videos = document.querySelectorAll(".video");
+
+videos.forEach((video, index) => {
+  video.addEventListener("ended", () => {
+    video.pause();
+
+    video.style.display = "none";
+
+    if (index < videos.length - 1) {
+      videos[index + 1].style.display = "block";
+      videos[index + 1].play();
+    } else {
+      videos[0].style.display = "block";
+      videos[0].play();
+    }
+  });
+});
